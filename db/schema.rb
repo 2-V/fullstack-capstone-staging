4260ad0707f892eb8973e2cb9dda18acc85376f7
Tim Filer <tim.j.filer@gmail.com>
2018-01-17 13:00:16 -0600

    Updated for module 1 submission

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180115172949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "foos", force: :cascade do |t|
+  create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
