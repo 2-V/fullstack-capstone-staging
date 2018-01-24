3f00bc3a294958427bc0cd02c3cc8564a3d93d76
Tim Filer <tim.j.filer@gmail.com>
2018-01-23 21:36:57 -0600

    Up to speed with active record

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
   end
+
+  get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
   
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
