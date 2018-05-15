201f3e6975241b0348c0a64d677801bd897b6d57
Tim Filer <tim.j.filer@gmail.com>
2018-05-15 15:29:42 -0500

    starting image content caching

---
+++ spec/factories/image_contents.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :image_content do
+    content_type "image/jpg"
+    content { File.open("db/images/sample.jpg","rb") {|f| Base64.encode64(f.read)}}
+    
+    original false
+  end
+end
--- spec/support/database_cleaners.rb
+++ spec/support/database_cleaners.rb
@@ -19,19 +19,35 @@ shared_context "db_scope" do
 	end
 end
 
+shared_context "db_clean_after" do
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
+  end
+end
+shared_context "db_clean_all" do
+  before(:all) do
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
+  end
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
+  end
+end
+
 shared_context "db_cleanup_each" do |ar_strategy=:truncation|
-	before(:all) do
-		DatabaseCleaner[:active_record].strategy = ar_strategy
-		DatabaseCleaner[:mongoid].strategy = :truncation
-		DatabaseCleaner.clean_with(:truncation)
-	end
-	after(:all) do
-		DatabaseCleaner.clean_with(:truncation)
-	end
-	before(:each) do
-		DatabaseCleaner.start
-	end
-	after(:each) do
-		DatabaseCleaner.clean
-	end
+  before(:all) do
+    DatabaseCleaner[:mongoid].strategy = :truncation, {:except => %w(locations)}
+    DatabaseCleaner[:active_record].strategy = ar_strategy
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
+  end
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
+  end
+  before(:each) do
+    DatabaseCleaner.start
+  end
+  after(:each) do
+    Capybara.reset_sessions!  if self.class.metadata[:js] 
+    #page.driver.clear_memory_cache
+    DatabaseCleaner.clean
+  end
 end
