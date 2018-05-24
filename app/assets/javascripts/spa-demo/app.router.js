a418921afdb793f4aadb79b1abe0d85d97a28584
Tim Filer <tim.j.filer@gmail.com>
2018-05-23 21:49:51 -0500

    Starting image/thing sync

--- app/assets/javascripts/spa-demo/app.router.js
+++ app/assets/javascripts/spa-demo/app.router.js
@@ -12,10 +12,8 @@
   function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
     $stateProvider
     .state("home",{
-      url: "/",
-      templateUrl: APP_CONFIG.main_page_html,
-      // controller: ,
-      // controllerAs: ,
+      url: "/subjects",
+      templateUrl: APP_CONFIG.subjects_page_html
     })
     .state("accountSignup",{
       url: "/signup",
@@ -32,10 +30,6 @@
     .state("things",{
       url: "/things/:id",
       templateUrl: APP_CONFIG.things_page_html
-    })
-    .state("subjects",{
-      url: "/subjects",
-      templateUrl: APP_CONFIG.subjects_page_html
     })
     ; 
 
